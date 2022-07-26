b51c624ad9ebd9efd2ab0b8abc7620380656a2f1
tomas.jeferson94@gmail.com <tomas.jeferson94@gmail.com>
2022-07-26 00:02:41 +0000

    Primeiro Commit

---
+++ Aula01/primeiros-comandos.sql
@@ -0,0 +1,14 @@
+-- Databricks notebook source
+select "Olá mundo!"
+
+-- COMMAND ----------
+
+SELECT 2 + 2
+
+-- COMMAND ----------
+
+SELECT 'INTERVALO NOW'
+
+-- COMMAND ----------
+
+SELECT 'TOMATE'
